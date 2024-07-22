6e2cccec84831504ab75c50fc7738dc84996f996
email <obansal.kunalo@gmail.com>
2024-07-22 17:38:59 +0530

    complete profile page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -65,7 +65,7 @@ export const google = async (req, res, next) => {
           Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
-        avatar: req.body.photoUrl,
+        avatar : req.body.photo,
       });
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
     },
     avatar:{
       type : String,
-      default : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC4_CgvYv5JCG8594upEO9urugbCcsJPgxxmBxQKwkPw&s'
+      default : 'https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg'
     },
 
   },
