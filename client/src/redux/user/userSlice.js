911028ff03e330c4292018d786607c4cf9d71a08
email <obansal.kunalo@gmail.com>
2024-07-31 16:22:30 +0530

    add delete user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -37,6 +37,19 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    deleteUserStart :(state) =>{
+      state.loading=true;
+    },
+    
+    deleteUserSuccess : (state) =>{
+  state.currentUser = null;
+  state.loading = false;
+  state.error=null;
+    },
+    deleteUserFailure :(state,action)=>{
+      state.error = action.payload;
+      state.error = false;
+    }
   },
 });
 
@@ -47,6 +60,9 @@ export const {
   updateUserFailure,
   updateUserSuccess,
   updateUserStart,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess
 } = userSlice.actions;
 
 export default userSlice.reducer;
