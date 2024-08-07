e7ee569b67c4888c15b1fd2e6da65c7779ce6337
email <obansal.kunalo@gmail.com>
2024-08-07 15:19:58 +0530

    create update lsiting api route

--- api/index.js
+++ api/index.js
@@ -36,7 +36,8 @@ app.use('/api/listing',listingRouter)
 app.use((err,req ,res ,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
-console.log(err.statusCode);
+    console.error(err);
+// console.log(err.statusCode);
     return res.status(statusCode).json({
         success: false,
         statusCode,
