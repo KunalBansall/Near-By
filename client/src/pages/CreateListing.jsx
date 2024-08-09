24bef944c12446f4c1472041a25379dbc4f3ca18
email <obansal.kunalo@gmail.com>
2024-08-09 17:07:18 +0530

    complete Listing Page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -286,7 +286,11 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
+              {formData.type==='rent' && (
+                
                 <span className="text-xs">(Rs / Month)</span>
+              )}
+                
               </div>
             </div>
             {formData.offer && (
@@ -303,7 +307,10 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">(Rs / Month)</span>
+                  {formData.type==='rent' && (
+                
+                <span className="text-xs">(Rs / Month)</span>
+              )}
                 </div>
               </div>
             )}
