f41b41869a26cfc15cac965a32523ec077f3d505
email <obansal.kunalo@gmail.com>
2024-08-13 10:22:53 +0530

    Add on Change and onSubmit on Listings page funtionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,29 +1,28 @@
 import { FaSearch } from "react-icons/fa";
-import { Link ,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  const [searchTerm ,setSearchTerm]=useState('');
-const navigate=useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
-  const handleSubmit=(e)=>{
-e.preventDefault();
-const urlParams = new URLSearchParams(window.location.search);
-urlParams.set('searchTerm',searchTerm);
-const searchQuery= urlParams.toString();
-navigate(`/search?${searchQuery}`);
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
   };
-  useEffect(()=>{
-    const urlParams= new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    if(searchTermFromUrl){
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
-  },[location.search]);
-  
+  }, [location.search]);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -33,17 +32,19 @@ navigate(`/search?${searchQuery}`);
             <span className="text-slate-700">By</span>
           </h1>
         </Link>
-        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search... "
             className="bg-transparent focus:outline-none w-24 sm:w-64 "
             value={searchTerm}
-            onChange={(e)=>setSearchTerm(e.target.value)}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
-
-          <FaSearch  className="text-slate-500" />
+            <FaSearch className="text-slate-500" />
           </button>
         </form>
         <ul className="flex gap-4 ">
@@ -59,9 +60,13 @@ navigate(`/search?${searchQuery}`);
             </li>
           </Link>
 
-          <Link to='/profile'>
+          <Link to="/profile">
             {currentUser ? (
-              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
             ) : (
               <li className=" text-slate-700 hover:underline ">Sign-in</li>
             )}
