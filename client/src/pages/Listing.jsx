92b67862c55b353e3e55dc55852600913082ed89
email <obansal.kunalo@gmail.com>
2024-08-14 15:54:54 +0530

    Create the Listing Items components and show listings

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -91,7 +91,7 @@ export default function Listing() {
               {listing.name} - ${' '}
               {listing.offer
                 ? listing.discountedPrice.toLocaleString('en-US')
-                : listing.regularPrice.toLocaleString('en-US')}
+                : listing.regularPrice.toLocaleString('en-US')} 
               {listing.type === 'rent' && ' / month'}
             </p>
             <p className='flex items-center mt-2 gap-2 text-slate-600  text-sm'>
@@ -105,7 +105,7 @@ export default function Listing() {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountedPrice}
+                  ${+listing.regularPrice - +listing.discountedPrice} OFF
                 </p>
               )}
             </div>
