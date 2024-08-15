1bccbc8861f5fd7030bc125e3cee6f803c78f7ef
email <obansal.kunalo@gmail.com>
2024-08-15 23:52:13 +0530

    show more listing functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Listingitems from "../components/Listingitems";
 
 export default function Search() {
   const navigate = useNavigate();
+  const location = useLocation();
   const [sidebardata, setSidebardata] = useState({
     searchTerm: "",
     type: "all",
@@ -16,7 +17,10 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings);
+  const [showMore ,setShowMore] = useState(false);
+   
+   
+
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -50,9 +54,15 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if(data.length>8){
+        setShowMore (true);
+      }else{
+        setShowMore(false);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -106,6 +116,21 @@ export default function Search() {
     urlParams.set("order", sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async ()=>{
+              const numberOfListings = listings.length;
+              const startIndex = numberOfListings;
+              const urlParams=new URLSearchParams(location.search);
+              urlParams.set('startIndex',startIndex);
+              const searchQuery = urlParams.toString();
+
+              const res = await fetch(`/api/listing/get? ${searchQuery}`);
+              const data = await res.json();
+              if(data.length <9){
+                setShowMore(false);
+              }
+              setListings([...listings, ...data]);
   };
   return (
     <div className="flex flex-col md:flex-row">
@@ -235,6 +260,15 @@ export default function Search() {
             <Listingitems key={listing._id} listing={listing} />
           ))
           }
+          {showMore && (
+                 
+              <button
+              onClick={ onShowMoreClick} 
+              className="text-green-700 hover:underline p-3 text-center w-full">
+                Show More
+              </button>
+
+          )}
       </div>
 
       </div>
