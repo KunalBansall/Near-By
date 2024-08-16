7f8fe05089a1b5264f3c4bec1e74f5040ed9547b
email <obansal.kunalo@gmail.com>
2024-08-16 13:15:43 +0530

    adding pre Installations

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
+
 
 
 mongoose
@@ -16,6 +18,7 @@ mongoose
 .catch((err)=>{
     console.log(err);
 });
+const __dirname = path.resolve();
 
 const app=express();
  app.use(express.json());
@@ -30,14 +33,18 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
-app.use('/api/listing',listingRouter)
+app.use('/api/listing',listingRouter);
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 app.use((err,req ,res ,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     console.error(err);
-// console.log(err.statusCode);
+
     return res.status(statusCode).json({
         success: false,
         statusCode,
