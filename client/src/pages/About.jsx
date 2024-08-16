f8875458f9f991c135797cbe6aa420a5d66a1b6e
email <obansal.kunalo@gmail.com>
2024-08-16 12:55:33 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>this is our About</div>
+    <div  className='py-20 px-4 max-w-6xl mx-auto' >
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Near-By</h1>
+      <p className='mb-4 text-slate-700'>
+      Near-By is a premier real estate platform designed to make the home-buying and selling experience seamless and accessible. With a comprehensive database of properties, we cater to diverse needs, whether you're looking for a cozy rental, a luxurious estate, or a prime commercial space. Our user-friendly interface and detailed property listings ensure that you have all the information you need to make an informed decision, all in one place.
+      </p>
+
+
+<p className='mb-4 text-slate-700'>
+Our mission is to revolutionize the real estate industry by providing unparalleled service and accessibility. We aim to empower individuals and families to find their ideal homes with ease and confidence. Through cutting-edge technology, expert market analysis, and personalized customer service, Near-By strives to simplify the real estate process, making it more transparent and efficient for everyone.
+</p>
+
+<p className='mb-4 text-slate-700'>
+At Near-By, our team is our greatest asset. Comprising seasoned real estate professionals, tech innovators, and customer service experts, our team works tirelessly to support you through every step of your real estate journey. With years of experience in the industry, our team members possess the knowledge and expertise to guide you through the complexities of the market. We are committed to understanding your unique needs and providing tailored solutions that help you achieve your real estate goals.
+      </p>
+    </div>
   )
 }
